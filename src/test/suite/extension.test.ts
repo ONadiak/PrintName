301faab799e5fcf028148dcb33bff3c0367c3039
onadiak <nadiak200202@gmail.com>
2023-11-24 16:46:14 +0200

    Fixed tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,18 +1,14 @@
 import * as vscode from 'vscode';
 import * as sinon from 'sinon';
-import { activate } from '../../extension';
 import { errorMessage } from '../../constants/messageConstants';
+const { describe, it, beforeEach, afterEach } = require('mocha'); 
 
 describe('Extension Activation', () => {
   let sandbox: sinon.SinonSandbox;
-  let fakeContext: vscode.ExtensionContext;
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
-    fakeContext = ({
-      subscriptions: [],
-    }) as unknown as vscode.ExtensionContext;
-  });
+    });
 
   afterEach(() => {
     sandbox.restore();
@@ -20,24 +16,42 @@ describe('Extension Activation', () => {
 
   it('should show information message if valid names are provided', async () => {
     const showInputBoxStub = sandbox.stub(vscode.window, 'showInputBox');
+
     showInputBoxStub.onFirstCall().resolves('Ostap');
     showInputBoxStub.onSecondCall().resolves('Nadiak');
+
     const showInformationMessageSpy = sandbox.spy(vscode.window, 'showInformationMessage');
 
-    await activate(fakeContext);
-
+    await vscode.commands.executeCommand("printName.helloWorld");
+    
     sinon.assert.calledTwice(showInputBoxStub);
     sinon.assert.calledWith(showInformationMessageSpy, 'Hello Ostap Nadiak');
   });
 
   it('should show error message if invalid names are provided', async () => {
     const showInputBoxStub = sandbox.stub(vscode.window, 'showInputBox');
+
     showInputBoxStub.onFirstCall().resolves('Ostap@');
     showInputBoxStub.onSecondCall().resolves('Nadiak');
+
     const showErrorMessageSpy = sandbox.spy(vscode.window, 'showErrorMessage');
 
-    await activate(fakeContext);
+    await vscode.commands.executeCommand("printName.helloWorld");
 
+    sinon.assert.calledTwice(showInputBoxStub);
+    sinon.assert.calledWith(showErrorMessageSpy, errorMessage);
+  });
+
+  it('should show error message if names are bigger than 15 symbols', async () => {
+    const showInputBoxStub = sandbox.stub(vscode.window, 'showInputBox');
+  
+    showInputBoxStub.onFirstCall().resolves('OstapBiggerThanAllowed');
+    showInputBoxStub.onSecondCall().resolves('Nadiak');
+  
+    const showErrorMessageSpy = sandbox.spy(vscode.window, 'showErrorMessage');
+  
+    await vscode.commands.executeCommand("printName.helloWorld");
+  
     sinon.assert.calledTwice(showInputBoxStub);
     sinon.assert.calledWith(showErrorMessageSpy, errorMessage);
   });
